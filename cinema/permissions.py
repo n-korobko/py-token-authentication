ff39d31045b7dc07ccdc7f8ee51270cd90a23299
Nikita <hkorobko@gmail.com>
2026-01-05 18:11:11 +0200

    Token Authentication

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -3,15 +3,14 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     """
-    The request is authenticated as an admin - read/write,
-    if as a user - read only request.
+    The request is authenticated as an admin — read/write.
+    If authenticated user — read only.
     """
 
     def has_permission(self, request, view):
-        is_readonly = (
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
+        is_readonly = request.method in SAFE_METHODS and (
+            request.user
+            and request.user.is_authenticated
         )
 
         is_admin = request.user and request.user.is_staff
